a3acb55b344eb9528c7e9d7debcfd56726c4257f
Elias6970 <eliasiborra12@gmail.com>
2024-10-30 20:26:04 +0100

    Deleted test prints

--- main.py
+++ main.py
@@ -36,14 +36,11 @@ async def insert_member(member:discord.Member):
     #need_img_save = True
 
     image_path = "to_check_avatar.png"
-    print(member.avatar.url) #type:ignore
     correct_download = await ImageManager.save_avatar_local(member.avatar.url,image_path) #type:ignore
     image_hash = calculate_hash(image_path)
 
     try:
-        print(member.name)
         last_user,last_name,last_image_path,last_image_hash = db.get_last_info(member.name) # Can throw error if the db is empty or the user doesn't exists
-            
 
         if correct_download == False:
             return False
@@ -74,13 +71,11 @@ async def insert_member(member:discord.Member):
 async def on_member_update(before:discord.Member,after:discord.Member):
     print("Changes detected")
     await insert_member(after)
-    print("\n")
+    print()
 
 
 @client.event
 async def on_ready():
-    
-
     print(f'Logged in as {client.user}!')
     
 
